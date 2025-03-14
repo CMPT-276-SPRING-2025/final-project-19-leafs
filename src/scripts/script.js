ad64049d1cc45891005b20cb378413bf09c6c881
navjot <nsn8@sfu.ca>
2025-03-13 18:18:41 -0700

    Added functionality to the chatbot toggle button using js

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+const chatbotToggler = document.querySelector("#chatbot-toggler");
+
+
+
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
