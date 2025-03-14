708cecb00a7c915f6e4ee13a1b291bf918ca3b1a
navjot <nsn8@sfu.ca>
2025-03-13 19:26:12 -0700

    Added the functionality to toggle the emoji menu from the emoji button in chatbot footer

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,25 @@
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 
+// Initialize emoji picker and handle emoji selection
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const { selectionStart: start, selectionEnd: end } = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    },
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        } else {
+            document.body.classList.remove("show-emoji-picker");
 
+        }
+    }
+});
 
+document.querySelector(".chat-form").appendChild(picker);
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
 closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
