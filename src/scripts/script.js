2c648ed747c2b916f621c539506d6f2c7b70f87d
navjot <nsn8@sfu.ca>
2025-03-16 00:40:53 -0700

    Displayed typed message in the body after clicking enter

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,33 @@
+const chatBody = document.querySelector(".chat-body");
+const messageInput = document.querySelector(".message-input");
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
+
+// Create message element with dynamic classes and return it
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+// Handle outgoing user messages
+const handleOutgoingMessage = (userMessage) => {
+    
+    // Create and display user message
+    const messageContent = `<div class="message-text">${userMessage}</div>`;
+
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+//Handle Enter key press for sending messages
+messageInput.addEventListener("keydown", (e) => {
+    const userMessage = e.target.value.trim();
+    if (e.key === "Enter" && userMessage) {
+        handleOutgoingMessage(userMessage);
+    }
+});
 
 // Initialize emoji picker and handle emoji selection
 const picker = new EmojiMart.Picker({
